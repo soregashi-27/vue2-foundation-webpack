aede6fb047245b4a916684b16f35033bba3fcbec
soregashi-27 <tomo.soul.15@gmail.com>
2021-09-03 13:16:20 +0900

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+
+module.exports = {
+  mode: 'none',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.vue$/, loader: 'vue-loader' },
+      { test: /\.css$/, loader: ['vue-style-loader', 'css-loader'] },
+    ],
+  },
+  plugins: [new VueloaderPlugin()],
+  resolve: {
+    alias: {
+      vue$: 'vue/dist/vue.esm.js',
+    },
+  },
+};
