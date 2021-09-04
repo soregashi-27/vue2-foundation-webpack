e93fbd0c3210f93be483f33c9465a37da4770448
soregashi-27 <tomo.soul.15@gmail.com>
2021-09-04 10:43:09 +0900

    propsを呼び出し

---
+++ src/components/AnchorLink.vue
@@ -0,0 +1,25 @@
+<template>
+  <span class="anchor" @click="onClickLink">{{ text }}</span>
+</template>
+
+<script>
+export default {
+  props: {
+    text: String,
+    href: String
+  },
+  methods: {
+    onClickLink() {
+      window.location.href = this.href
+    }
+  }
+}
+</script>
+
+<style scoped>
+.anchor {
+  color: blue;
+  cursor: pointer;
+  text-decoration: underline;
+}
+</style>
