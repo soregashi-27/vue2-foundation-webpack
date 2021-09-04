16ca8f99800f3b0af1a17476f2c8c9b1594abb21
soregashi-27 <tomo.soul.15@gmail.com>
2021-09-04 12:59:10 +0900

    イベント発行の流れを追加

--- src/index.js
+++ src/index.js
@@ -2,10 +2,29 @@ import Vue from 'vue'
 // import Hello from './components/Hello.vue' //componentsの動き用に追加
 
 import AnchorLink from './components/AnchorLink.vue'
+import EventButtons from './components/EventButtons.vue'
 
 new Vue({
   el: '#app',
+  data: {
+    arguments: []
+  },
   components: {
-    AnchorLink
+    AnchorLink,
+    EventButtons
+  },
+  methods: {
+    onEventOne() {
+      alert('Event 1')
+    },
+    onEventTwo(argument) {
+      this.arguments = []
+      this.rguments.push(argument)
+    },
+    oneventThree(arg1, arg2) {
+      this.arguments = []
+      this.arguments.push(arg1)
+      this.arguments.push(arg2)
+    }
   }
 })
